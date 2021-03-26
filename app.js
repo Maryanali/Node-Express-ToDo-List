2fee8c779acdcde400cd6ccb6b4430603297fac3
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-03-26 11:36:21 +0000

    Finished the logic so now the ToDo List is working.

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
 //jshint esversion:6
 
 const express =require("express");
-const body = require("body-parser");
+const bodyParser = require("body-parser");
 const app= express();
+var items= [];//an array so we can append the item to the array.
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-// uses the view engine to render a particular page called index.
-//that is an index.ejs page.
-var today = new Date();
-var currentDay = today.getDay();
-var day ="";
-// switch (currentDay){
-//     case 0:
-//         day= "Sunday";
-//         break;
-//     case 1:
-//         day= "Monday";
-//         break;
-//     case 2:
-//         day= "Tuesday";
-//         break;
-//     case 3:
-//         day= "Wednesday";
-//         break;
-//     case 4:
-//         day= "Thursday";
-//         break;
-//     case 5:
-//         day= "Friday";
-//         break;
-//     case 6:
-//         day= "Saturday";
-//         break;
-//     default:
-//     console.log("Error: current day is: " + currentDay);
-// } 
-res.render("list", {kindOfDay:day});
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res){
+
+var today = new Date();
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+var day = today.toLocaleDateString("en-US", options);
+res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+//handle post requests to the home route
+app.post("/", function(req,res){
+ var item= req.body.newItem;
+ items.push(item);
+ //redirect to home route, triggers app.get which res.renders which will pass it both the kind of day and the new list item.
+ res.redirect("/");
 });
 
 app.listen(3000, function(){
