6c8fc38076faaafb246906513e2b995847432d6a
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-03-26 13:10:29 +0000

    module.

--- date.js
+++ date.js
@@ -2,7 +2,8 @@
 
 module.exports = getDate;
 //no parenthesis because not calling the function
-
+//module.export is a javascript object, so we can transport more than one function.
+//module.exports.getDate=getDate; then use another one for day module.exports.getDay;
 function getDate(){
 
 let today = new Date();
